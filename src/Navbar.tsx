eda8db368b71fc55ece488d8c499759e2d30429c
rishu12911 <107927397+rishu12911@users.noreply.github.com>
2024-05-12 16:42:11 +0530

    Update Navbar.tsx

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import reactLogo from './assets/react.svg'
+
 
 
 const Navbar: React.FC = () => {
+  
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <div className="navbar-left">
-          <img src={reactLogo} alt="Logo" className="logo" />
-          <span className="navbar-brand">E/G Articles</span>
-        </div>
-        <div className="navbar-right">
-          <button className="nav-button">Find Your Path</button>
-          
-        </div>
+        
+        
+        <span className="navbar-center">E/G Articles</span>
+        
+       
+        
       </div>
     </nav>
   );
